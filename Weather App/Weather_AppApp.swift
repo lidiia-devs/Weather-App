f408600b43d124b5aaa9ef2d704fd54b345d0809
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-02-03 21:10:33 -0500

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Lidiia Diachkovskaia on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
